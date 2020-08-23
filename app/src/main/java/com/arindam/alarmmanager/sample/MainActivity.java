cd22b77266bd0faef440272f52c60131b2d64723
arindam2ghosh <arindam2ghosh@gmail.com>
2020-08-23 19:59:52 +0800

    7th update

--- app/src/main/java/com/arindam/alarmmanager/sample/MainActivity.java
+++ app/src/main/java/com/arindam/alarmmanager/sample/MainActivity.java
@@ -11,7 +11,9 @@ import com.arindam.alarmmanager.database.AlarmCursorWrapper;
 import com.arindam.alarmmanager.model.Alarm;
 
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setReminder();
+        setPeriodicReminder();
     }
 
     private void setReminder(){
@@ -36,9 +38,36 @@ public class MainActivity extends AppCompatActivity {
 
         Alarm alarm = new Alarm();
         alarm.setExternalId(10);
-        alarm.setExact(false);
-        alarm.setType(Alarm.RTC_WAKEUP);
+        alarm.setPeriodic(false);
         alarm.setTime(calendar.getTimeInMillis());
+        alarm.setEnabled(true);
+        alarm.setTitle("Title");
+        alarm.setMessage(getString(R.string.app_name));
+        alarm.setAlarmHandleListener(SendNotification.class.getCanonicalName());
+
+        new ArinAlarmController().setAlarm(MainActivity.this, alarm);
+
+    }
+
+    private void setPeriodicReminder(){
+
+        List<Alarm> alarmList = new ArinAlarmController().getAllAlarm(MainActivity.this);
+        Log.i("Arindam", ""+alarmList.size());
+
+        new ArinAlarmController().deleteAllAlarm(MainActivity.this);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), 0, 0, 0);
+//        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.add(Calendar.MINUTE, 1);
+
+
+        Alarm alarm = new Alarm();
+        alarm.setExternalId(10);
+        alarm.setPeriodic(true);
+        alarm.setPeriodicType(Alarm.TIMEUNIT_MINUTE);
+        alarm.setPeriodicValue(10);
+        alarm.setPeriodicStartTime(calendar.getTimeInMillis());
         alarm.setEnabled(true);
         alarm.setTitle("Title");
         alarm.setMessage(getString(R.string.app_name));
